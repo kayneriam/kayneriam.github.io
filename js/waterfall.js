edef1a6e3ca10eb7a9aa751cb7a91cff14061bc6
kayneriam <kayner.iam@gmail.com>
2020-03-22 17:24:09 +0300

    [Demo] Added git-switch demo

--- js/waterfall.js
+++ js/waterfall.js
@@ -1,5 +1,13 @@
 (function() {
   var demoContent = [
+    {
+      demo_link: 'https://github.com/kayneriam/git-switch',
+      img_link: 'https://i.loli.net/2020/03/22/T8YbCeDMvlOyQud.png',
+      code_link: 'https://github.com/kayneriam/git-switch',
+      title: 'git-switch',
+      core_tech: 'Ruby, git',
+      description: 'Tool for management git accounts'
+    }
   ];
 
   contentInit(demoContent)
@@ -10,7 +18,7 @@ function contentInit(content) {
 
   var htmlStr = ''
   for (var i = 0; i < content.length; i++) {
-    htmlStr += '<div class="grid-item">' + '   <a class="a-img" href="' + content[i].demo_link + '">' + '       <img src="' + content[i].img_link + '">' + '   </a>' + '   <h3 class="demo-title">' + '       <a href="' + content[i].demo_link + '">' + content[i].title + '</a>' + '   </h3>' + '   <p>Stack: ' + content[i].core_tech + '</p>' + '   <p>' + content[i].description + '       <a href="' + content[i].code_link + '"><p>Source <i class="fa fa-code" aria-hidden="true"></i></p></a>' + '   </p>' + '</div>'
+    htmlStr += '<div class="grid-item">' + '   <a class="a-img" href="' + content[i].demo_link + '">' + '       <img src="' + content[i].img_link + '">' + '   </a>' + '   <h3 class="demo-title">' + '       <a href="' + content[i].demo_link + '">' + content[i].title + '</a>' + '   </h3>' + '   <p>Core tech: ' + content[i].core_tech + '</p>' + '   <p>' + content[i].description + '       <a href="' + content[i].code_link + '"><p>Source <i class="fa fa-code" aria-hidden="true"></i></p></a>' + '   </p>' + '</div>'
   }
   var grid = document.querySelector('.grid')
   grid.insertAdjacentHTML('afterbegin', htmlStr)
